f2ce7dc63faad9e1405d01277fd4230e9568dfc0
Frank Jackson <fjay45@Franks-MacBook-Pro.local>
2021-10-27 17:07:54 -0700

    First revision of 3 Card Spread GUI complete.

--- bin/pt_3card_gui.py
+++ bin/pt_3card_gui.py
@@ -1,27 +1,43 @@
-from random import *
+import random
 from tkinter import *
 from tkinter import ttk
 
 #Path to the pack in use:
 pack = "../img/packs/rwps"
 
-def shuffle_cards():
-    deck = []
+def get_orientation():
     #Upright or Reversed Indicator
-    ur = randint(0,1)
+    ur = random.randint(0,1)
     #
     if ur == 0:
-        upright_or_reversed = "upright" 
+        orientation = "upright" 
     else:
-        upright_or_reversed = "reversed" 
+        orientation = "reversed" 
+    return orientation
+       
+def shuffle_cards():
+    deck = []
+    card = {}
+    name = ""
+    for c in range(22):
+        if c < 10:
+            name = "ar0" + str(c) + ".gif"
+        else:
+            name = "ar" + str(c) + ".gif"
+        card['name'] = name 
+        card['orientation'] = get_orientation()
+        deck.append(card)
+        card = {}
 
-    for card in range(0,21):
-        pass
+    random.shuffle(deck)
+    return deck
 
 def flip_cards():
-    flipped_card1 = PhotoImage(file=pack + '/upright/ar00.gif')
-    flipped_card2 = PhotoImage(file=pack + '/upright/ar01.gif')
-    flipped_card3 = PhotoImage(file=pack + '/upright/ar02.gif')
+    shuffled_deck = shuffle_cards()
+    print(shuffled_deck)
+    flipped_card1 = PhotoImage(file=pack + '/' + shuffled_deck[0]['orientation'] + '/' + shuffled_deck[0]['name'])
+    flipped_card2 = PhotoImage(file=pack + '/' + shuffled_deck[1]['orientation'] + '/' + shuffled_deck[1]['name'])
+    flipped_card3 = PhotoImage(file=pack + '/' + shuffled_deck[2]['orientation'] + '/' + shuffled_deck[2]['name'])
     card_frame1.configure(image=flipped_card1)
     card_frame1.image = flipped_card1
     card_frame2.configure(image=flipped_card2)
